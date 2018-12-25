794beac68dc46e97884e9aa5e0da3f5333d3bb3c
joanripe <joanripe@gmail.com>
2018-12-25 13:42:37 +0100

    generador de nivel
    implementadas las bases para poder empezar a spawnear piezas en el juego

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { set; get; }
+
+    // spawn del nivel
+
+
+    //lista de piezas
+
+    public List<Piece> ramps = new List<Piece>();
+    public List<Piece> longblocks = new List<Piece>();
+    public List<Piece> jumps = new List<Piece>();
+    public List<Piece> slides = new List<Piece>();
+    public List<Piece> pieces = new List<Piece>(); // todas las piezas en el pool
+
+    public Piece GetPiece (PieceType _pt, int visualIndex)
+    {
+        Piece p = pieces.Find(x => x.type == _pt && x.visualIndex == visualIndex && !x.gameObject.activeSelf);
+
+        if (p == null)
+        {
+            GameObject go = null;
+            if (_pt == PieceType.ramp)
+            {
+                go = ramps[visualIndex].gameObject;
+            }
+            else if (_pt == PieceType.longBlock)
+            {
+                go = longblocks[visualIndex].gameObject;
+            }
+            else if (_pt == PieceType.jump)
+            {
+                go = jumps[visualIndex].gameObject;
+            }
+            else if (_pt == PieceType.slide)
+            {
+                go = slides[visualIndex].gameObject;
+            }
+
+            go = Instantiate(go);
+            p = go.GetComponent<Piece>();
+            pieces.Add(p);
+        }
+
+        return p;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PieceSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceSpawner : MonoBehaviour
+{
+
+    public PieceType type;
+    private Piece currentPiece;
+    
+    public void Spawn()
+    {
+        currentPiece = LevelManager.Instance.GetPiece(type, 0); //obtener una nueva pieza del pool
+        currentPiece.gameObject.SetActive(true);
+        currentPiece.transform.SetParent(transform, false);
+    }
+
+    public void DeSpawn()
+    {
+        currentPiece.gameObject.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
